432569a7bbf248963b1ff921228b4ad3a8345835
AndrejPitomec <148689748+AndrejPitomec@users.noreply.github.com>
2023-12-22 09:22:23 +0100

    Add files via upload

---
+++ menuvacnica/src/menuvacnica.java
@@ -0,0 +1,14 @@
+public class menuvacnica {
+
+	public static void main(String[] args) {
+	
+	double EUR = (61.7);
+	int DEN = 50;
+	double REZ = (EUR * DEN);
+
+	System.out.println ("Vkupno vo denari:");
+	System.out.print (REZ);
+	}
+
+}
+
